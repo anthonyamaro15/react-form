1a7c1dc901a6ffb958404b7017cda56bc29bf16c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-18 20:05:20 -0700

    finished with the styling for the signup component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUp from "./components/SignUp";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import { NavLink } from "react-router-dom";
+
+const SignUp = () => {
+  return (
+    <div className="container">
+      <div className="toggle">
+        <NavLink to="signup" activeClassName="active">
+          sign up
+        </NavLink>
+        <NavLink to="login" activeClassName="active">
+          log ing
+        </NavLink>
+      </div>
+
+      <h2>sing up for free</h2>
+      <Form>
+        <div className="full-name">
+          <div className="fname">
+            <label>
+              first name
+              <Field type="text" name="fname" />
+            </label>
+          </div>
+          <div className="lname">
+            <label>
+              last name
+              <Field type="text" name="lname" />
+            </label>
+          </div>
+        </div>
+        <label>
+          email
+          <Field type="email" name="email" />
+        </label>
+        <label>
+          password
+          <Field type="password" name="password" className="max-width" />
+        </label>
+        <span>hello</span>
+
+        <label>
+          confirm password
+          <Field type="password" name="confirm" className="max-width" />
+          <span>hello</span>
+        </label>
+
+        <button type="submit">get started</button>
+      </Form>
+    </div>
+  );
+};
+
+export default withFormik({
+  mapPropsToValues: () => ({
+    fname: "",
+    lname: "",
+    password: "",
+    confirm: ""
+  })
+})(SignUp);
