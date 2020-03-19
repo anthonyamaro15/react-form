4a285341904a6811c43c93f9f139eb22e02a339c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-18 21:50:52 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
     <div>
-      <Route path="/signup" component={SignUp} />
-      <Route path="/login" component={Login} />
+      <Route exact path="/" component={Home} />
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
